616ca6304e32f8af101d4d7d497270736ba5ba96
gilyong <lgy910920@naver.com>
2016-11-16 23:32:30 +0900

    - 수정 깃 import 한글 테스트1

--- LibraryApp/src/swe/Main.java
+++ LibraryApp/src/swe/Main.java
@@ -4,9 +4,9 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		//Library.Start(); //
+		//Library.Start(); //싱글톤 적용시 사용
 		
-		System.out.println("�̱�� �׽�Ʈ2");
+		System.out.println("이길용 테스트2");
 	}
 
 }
