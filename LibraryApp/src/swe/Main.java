f073b57bae17eb0f809bcd3c6b73081079e7a517
김만규 <aksrb1030@naver.com>
2016-11-16 23:07:16 +0900

    - add test1 코드 추가함

--- LibraryApp/src/swe/Main.java
+++ LibraryApp/src/swe/Main.java
@@ -5,7 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		//Library.Start(); // 싱글톤 패턴 적용시 사용
-		 
+		 System.out.println("김만규 테스트1");
 	}
 
 }
